7f9e5c4ee3895a068c92fb4f02a024894e78e824
Luis Chapa <luis.chapa@trustvesta.com>
2020-12-12 15:43:23 -0600

    Comment Interface

--- src/com/anexinet/Solution.java
+++ src/com/anexinet/Solution.java
@@ -1,6 +1,19 @@
 package com.anexinet;
 
+
+/**
+ * Interface to be implemented for each single and specific problem asked to solve.
+ * 
+ * @author luis.chapa
+ *
+ */
 public interface Solution
-{
+{   
+    /**
+     * Method for solveing the problem 
+     *  
+     * @param testCase String Path to the TestCases file to run the solution of the problem
+     * @throws Exception Something goes wrong in the execution
+     */
     void solve(String testCase) throws Exception;
 }
